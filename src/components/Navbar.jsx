bc8423386eb4c9222d0a5381795c45e034023464
elisa <bascongarcia@gmail.com>
2022-09-20 11:07:38 +0200

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ export default function Navbar() {
         <div className='Logo'><img width="250px" src='./images/Logo.png' alt="logo"></img></div>
         <div>
           <ul>
+            <li><button className='unstyle' onClick={() => navigate(-1)}><span class="material-symbols-outlined">arrow_back_ios</span></button></li>
             <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/">Home</NavLink></li>
             {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/login">Login</NavLink></li>}
             {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/signup">Sign up</NavLink></li>}
-            <li><button onClick={() => navigate(-1)}>Go back</button></li>
-            {isLoggedIn && <li><button onClick={() => logOutUser()}>Log out</button></li>}
+            {isLoggedIn && <li><button className='unstyle' onClick={() => logOutUser()}><span class="material-symbols-outlined">logout</span></button></li>}
           </ul>
         </div>
       </nav>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
           {!isLoggedIn && <p className='Toast'>Sing Up or Login to find your perfect Gravel</p>}
           {!isLoggedIn && <p><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/login">Login</NavLink></p>}
           {!isLoggedIn && <p><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/signup">Sign up</NavLink></p>}
-          {isLoggedIn && <p><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/newreview">ADD NEW REVIEW</NavLink></p>}
+          {isLoggedIn && <p className='unstyle'><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/newreview">ADD NEW REVIEW</NavLink></p>}
           {isLoggedIn && <p>SEE MY BIKES</p>}
           {/* {isLoggedIn && <p><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/reviews">SEE ALL REVIEWS</NavLink></p>} */}
         </div>
