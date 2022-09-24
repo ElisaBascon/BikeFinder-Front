1017f64012e638b50b5d876b612e813418cb4534
elisa <bascongarcia@gmail.com>
2022-09-24 13:04:56 +0200

    Imorted all images to Cloudinary

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,11 @@ export default function Navbar() {
   return (
     <div>
       <nav>
-        <div className='Logo'><img width="250px" src='./images/Logo.png' alt="logo"></img></div>
+        <div className='Logo'><img width="250px" src='https://res.cloudinary.com/ds0q8dpvh/image/upload/v1664016869/bikeFinder/Logo_anovv9.png' alt="logo"></img></div>
         <div>
           <ul>
             <li><button className='unstyle' onClick={() => navigate(-1)}><span className="material-symbols-outlined">arrow_back</span></button></li>
-            <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/"><img src='/images/Home.png' alt='home'></img></NavLink></li>
+            <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/"><img src='https://res.cloudinary.com/ds0q8dpvh/image/upload/v1664016506/bikeFinder/Home_pz3d8n.png' alt='home'></img></NavLink></li>
             {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/login">Login</NavLink></li>}
             {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/signup">Sign up</NavLink></li>}
             {isLoggedIn && <li><button className='unstyle' onClick={() => logOutUser()}><span className="material-symbols-outlined">logout</span></button></li>}
