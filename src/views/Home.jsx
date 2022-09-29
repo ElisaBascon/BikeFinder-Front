7941475a99fceaf586cae430c7fd3d6812bc7b41
elisa <bascongarcia@gmail.com>
2022-09-29 11:58:28 +0200

    Home no logging styles Done

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,17 +10,28 @@ export default function Home() {
       <div className='grid-container'>
         <div className='item-1'>
 
-          <div className='LogIn-SignUp' >
-            {!isLoggedIn && <p><Link to="/login">Login</Link></p>}
-            {!isLoggedIn && <p><Link to="/signup">Sign Up</Link></p>}
-          </div>
-          
-          <div>
-            {user && <p className='Toast'>Hi {user.username}</p> }
-            {isLoggedIn && <p><Link className={(element) => element.isActive ? 'selected' : ''} to={`/favorites`}>FAVORITE BIKES</Link></p>}
-            {isLoggedIn && <p><Link className={(element) => element.isActive ? 'selected' : ''} to="/newreview">NEW REVIEW</Link></p>}
-            {isLoggedIn && <p><Link className={(element) => element.isActive ? 'selected' : ''} to={`/reviews/mine`}>MY REVIEWS</Link></p>}
-          </div>
+          {!isLoggedIn && <p><Link to="/login">Login</Link></p>}
+          {!isLoggedIn && <span className='login-signup-link'>⚡</span>}
+          {!isLoggedIn && <p><Link to="/signup">Sign Up</Link></p>}
+
+        {!isLoggedIn && <div className="textcirc">
+          <svg x="0px" y="0px"
+              viewBox="0 0 100 100" fill="transparent">
+          <path id="circt" d="M50,93C26.25,93,7,73.75,7,50S26.25,7,50,7s43,19.25,43,43c0,23.58-18.98,42.73-42.49,43
+              "/>
+          <text><textPath href="#circt" startOffset="0%">
+          <tspan className="white">Sign Up or Login to find your perfect Gravel
+          </tspan></textPath>
+          </text>
+          </svg>
+        </div>}
+
+        <div>
+          {user && <p className='Toast'>Hi {user.username}</p> }
+          {isLoggedIn && <p><Link className={(element) => element.isActive ? 'selected' : ''} to={`/favorites`}>FAVORITE BIKES</Link></p>}
+          {isLoggedIn && <p><Link className={(element) => element.isActive ? 'selected' : ''} to="/newreview">NEW REVIEW</Link></p>}
+          {isLoggedIn && <p><Link className={(element) => element.isActive ? 'selected' : ''} to={`/reviews/mine`}>MY REVIEWS</Link></p>}
+        </div>
 
         </div>
 
@@ -46,16 +57,16 @@ export default function Home() {
             {isLoggedIn && <Reviews/>}
             {!isLoggedIn && <div className="wrapper">
               <div className="mover-1">
-              ☞Sign Up or Login to find your perfect Gravel☜
+              ☞Start here your Gravel Adventure and share it☜
               </div>
               <div className="mover">
-              ☞Sign Up or Login to find your perfect Gravel☜  
+              ☞Start here your Gravel Adventure and share it☜
               </div>
               <div className="mover-1">
-              ☞Sign Up or Login to find your perfect Gravel☜
+              ☞Start here your Gravel Adventure and share it☜
               </div>
               <div className="mover">
-              ☞Sign Up or Login to find your perfect Gravel☜     
+              ☞Start here your Gravel Adventure and share it☜    
               </div>
             </div>}
             {!isLoggedIn && <div className='container-images-noLogin' >
